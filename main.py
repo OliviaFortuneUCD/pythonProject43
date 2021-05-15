c3b0c8acabdccb40115e92644cc6d4e3ca258df5
olivia.fortune@griffith.ie <olivia.fortune@griffith.ie>
2021-05-15 22:09:30 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+covid = pd.read_csv("COVID.csv")
+
+# Choose entries with county Name of Cork.
+Corkonly  = covid[covid['CountyName'] == 'Cork']
+Dublinonly  = covid[covid['CountyName'] == 'Dublin']
+
+
+print(Corkonly.to_string())
+print(Dublinonly.to_string())
