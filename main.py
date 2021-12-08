5e389516e263247696b42d7d3e8c8e89de6c547b
OliviaFortuneUCD <81123269+OliviaFortuneUCD@users.noreply.github.com>
2021-12-08 18:40:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ covid = pd.read_csv("COVID.csv")
 Corkonly  = covid[covid['CountyName'] == 'Cork']
 Dublinonly  = covid[covid['CountyName'] == 'Dublin']
 Belfast  = covid[covid['CountyName'] == 'Belfast']
+Limeick  = covid[covid['CountyName'] == 'Limerick']
 
 print(Corkonly.to_string())
 print(Dublinonly.to_string())
